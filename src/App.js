9eed7765b1342f0f1068951358fe510f064035b0
miri52 <miri52@users.noreply.github.com>
2020-06-17 16:57:11 +0100

    Set up a basic typing game
    use React Hooks to maintain state, setTimeout and to make changes to the textearea DOM node

--- src/App.js
+++ src/App.js
@@ -1,8 +1,65 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const STARTING_TIME = 10;
+
+  const [timeLeft, setTimeLeft] = useState(STARTING_TIME);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [text, setText] = useState("");
+  const [totalWords, setTotalWords] = useState(0);
+  const textareaRef = useRef(null);
+
+  function handleChange(event) {
+    const { value } = event.target;
+    setText(value);
+  }
+
+  function countWords(text) {
+    const wordsArr = text.trim().split(" ");
+    return wordsArr.filter((word) => word !== "").length;
+  }
+
+  function startGame() {
+    setIsTimeRunning(true);
+    setTimeLeft(STARTING_TIME);
+    setText("");
+    textareaRef.current.disabled = false;
+    textareaRef.current.focus();
+    setTotalWords(0);
+  }
+
+  function endGame() {
+    setIsTimeRunning(false);
+    setTotalWords(countWords(text));
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && timeLeft > 0) {
+      setTimeout(() => {
+        setTimeLeft((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (timeLeft === 0) {
+      endGame();
+    }
+  }, [timeLeft, isTimeRunning]);
+
+  return (
+    <div className="App">
+      <h1>How fast can you type?</h1>
+      <textarea
+        ref={textareaRef}
+        onChange={handleChange}
+        value={text}
+        disabled={!isTimeRunning}
+      />
+      <h3>Time left: {timeLeft} seconds </h3>
+      <button onClick={startGame} disabled={isTimeRunning}>
+        Start
+      </button>
+      <h3>Total words: {totalWords}</h3>
+    </div>
+  );
 }
 
 export default App;
